c04e078768c0a548879608f51a0bae22e1c03f8e
Lawrence <lawrence.zhou2010@gmail.com>
2022-02-15 09:50:17 -0500

    Added Calculator
    Both C and EXE added.

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
 	
-	printf("Hello world!");
+	bool escape = false;
+	
+	printf("Hello world!\n");
+	printf("Press space, then enter keys to exit.");
+	
+	while(!escape)
+	{
+		
+		if(getchar() == '\n')
+		{
+			
+			escape = true;
+			
+		}
+		
+	}
 	
 }
