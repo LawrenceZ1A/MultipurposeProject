ad89020cee724dfc760fe2c60fb3fa87a04f4787
CuboidRaptor <fanjas112358@gmail.com>
2022-02-16 15:29:32 -0500

    yay

--- cuboid_fight.py
+++ cuboid_fight.py
@@ -18,23 +18,64 @@ master = Tk()
 master["background"] = "#01FEAA"
 master.geometry("720x480")
 master.title("Fighting Cuboid")
+master.resizable(False, False)
 
 cdata, cldata = data, ldata
 var = StringVar()
 var2 = ""
+
+def textbox(text):
+    tdis = Text(
+        master,
+        width=700,
+        height=2
+    )
+    tdis.insert(
+        INSERT,
+        text
+    )
+    tdis.configure(state="disabled")
+    tdis.pack()
     
-text = Entry(master, width=700)
-text.insert(0, cldata[0])
-text.pack()
+    return tdis
 
 def selecta(i):
     global var, var2
     var.set(i)
     var2 = i
+    
+def button(texts, tbox):
+    global master
+    
+    select = []
+    for text in texts:
+        temp = Button(
+            master,
+            text=str(text),
+            command=lambda i=text: selecta(
+                i
+            )
+        )
+        temp.pack()
+        select.append(temp)
+        
+    master.wait_variable(var)
+    
+    for item in select:
+        item.destroy()
+        
+    tbox.destroy()
+    
+    return var2
 
-for item in cldata[1:]:
-    select = Button(master, text=str(item), command=lambda i=item: selecta(i))
-    select.pack()
-
-master.wait_variable(var)
-print(var2)
+while True:
+    tbox = textbox(cldata[0])
+    temp = button(cldata[1:], tbox)
+    cdata = cdata[temp]
+    cldata = list(cdata)
+    
+    if cdata == cldata:
+        textbox("`".join(cldata[0].split("`")[:~0]))
+        textbox(cldata[0].split("`")[~0])
+        
+        break
