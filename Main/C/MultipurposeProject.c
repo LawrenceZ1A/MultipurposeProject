76a0a4dff472d6e05a0fe0de05362935c94e4c2f
Lawrence <lawrence.zhou2010@gmail.com>
2022-04-19 14:37:36 -0400

    Fixed certain things and added disclaimer I guess.
    Yay now no one will die from lack of a disclaimer

--- Main/C/MultipurposeProject.c
+++ Main/C/MultipurposeProject.c
@@ -14,16 +14,16 @@ int main()
 	printf("C (C-Calculator)\n");
 	printf("T (CPSTest)\n");
 	printf("W (The Game of War)\n");
-	printf("M (Machine Learning)\n\n");
+	printf("M (Machine Learning (Disclamer! This is a software program that requires an extremely large amount of computer resources. During testing, our computers experienced maximum loads of up to 8GB of RAM and the program consumed around 50% of the 6-core CPU built-in our computer. It also consumed large amounts of other system resources and took around 30 minutes. Proceed at your own pace.) )\n\n");
 	
 	while(true)
 	{
 		
-		char input;
-		scanf("%c", &input);
+		char input1;
+		scanf("%c", &input1);
 		
 		chdir("..\\\\..");
-		if(input == 'C' || input == 'c')
+		if(input1 == 'C' || input1 == 'c')
 		{
 			
 			printf("\n\n");
@@ -31,23 +31,23 @@ int main()
 			return(0);
 			
 		}
-		else if(input == 'T' || input == 't')
+		else if(input1 == 'T' || input1 == 't')
 		{
 			
 			(void)system("CPSTest\\\\CPSTest.exe");
 			exit(0);
 			
 		}
-		else if(input == 'W' || input == 'w')
+		else if(input1 == 'W' || input1 == 'w')
 		{
 			
 			(void)system("run.bat");
 			exit(0);
 			
 		}
-		else if(input == 'M' || input == 'm')
+		else if(input1 == 'M' || input1 == 'm')
 		{
-			
+
 			system("Keras\\\\dist\\\\how-to-forecast\\\\how-to-forecast.exe");
 			exit(0);
 			
